dc22a7a30b36ce5024501d475b18c3dfcc6b5f6a
Shivank Chauhan <shichauh@publicisgroupe.net>
2018-04-09 17:17:50 +0530

    initial code commit

---
+++ src/main/java/com/howtoprogram/kafka/Listener.java
@@ -0,0 +1,20 @@
+package com.howtoprogram.kafka;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+
+public class Listener {
+
+	public final CountDownLatch countDownLatch1 = new CountDownLatch(1);
+
+	@KafkaListener(id = "foo", topics = "topic1", group = "group1")
+	public void listen(ConsumerRecord<?, ?> record) {
+		System.out.println(record);
+		Object payload = record.value();
+		System.out.println(paylo);
+		countDownLatch1.countDown();
+	}
+
+}
